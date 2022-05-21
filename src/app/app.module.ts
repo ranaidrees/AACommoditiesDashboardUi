a42a81d94c9ac9ef82d728ee66bdaa046568af5e
Rana Naveed Idrees <rana.idrees@rpc-tyche.com>
2022-05-21 16:17:36 +0100

    updating the structure of the UI componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { CardComponent } from './card/card.component';
 import { NgChartsModule } from 'ng2-charts';
-import { HistoricalPnlChartComponent } from './charts/historical-pnl-chart/historical-pnl-chart.component';
-import { MiniCardComponent } from './mini-card/mini-card.component';
+import { MiniCardComponent } from './dashboard/Indicators/mini-card/mini-card.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -26,6 +24,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { TradeActionTableComponent } from './dashboard/trade-action-table/trade-action-table.component';
+import { HistoricalPnlChartComponent } from './dashboard/Indicators/charts/historical-pnl-chart/historical-pnl-chart.component';
+import { CardComponent } from './dashboard/card/card.component';
 
 @NgModule({
   declarations: [
