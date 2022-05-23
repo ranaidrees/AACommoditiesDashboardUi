933ff69e97783d4f0ed99b0e41d3a0ee111523fa
Rana Naveed Idrees <rana.idrees@rpc-tyche.com>
2022-05-23 21:54:40 +0100

    Removed the nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -28,12 +27,10 @@ import { HistoricalLineChartComponent } from './dashboard/Indicators/charts/hist
 import { CardComponent } from './dashboard/card/card.component';
 import { AuthenticationModule } from './shared';
 import { LoginModule } from './login/login.module';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavComponent,
         DashboardComponent,
         CardComponent,
         HistoricalLineChartComponent,
